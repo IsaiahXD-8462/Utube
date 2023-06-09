05ac124ff23aa811bcda36be08c08aba1abe2747
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-06-09 17:01:38 -0400

    Altered related videos component to show other vidoes

--- frontend/src/components/Related Videos/RelatedVideos.jsx
+++ frontend/src/components/Related Videos/RelatedVideos.jsx
@@ -1,12 +1,25 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
+import VideoPlayer from "./"
 
-const Results = (props) => {
+const RelatedVideos = (props) => {
     
+   const [video, setVideo] = useState([]);
     
-    return ( 
-        <div>Hi</div>
+    async function getallvideos() {
+    const response = await axios.get('http://127.0.0.1:8000/api/auth/comments/comment/all')
+    try {response =>
+      console.log(response.data);
+      setVideos(response.data)}
+    catch {error =>
+      console.log(error)
+    };
+  }
+   return ( 
+        <div>
+            <VideoPlayer></VideoPlayer>
+        </div>
      );
 }
  
-export default Results;
+export default RelatedVideos;
