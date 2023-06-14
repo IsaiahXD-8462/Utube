a36d1ce8c22c56d6849d208c9cf15baf9b346eb5
IsaiahXD-8462 <116024098+IsaiahXD-8462@users.noreply.github.com>
2023-06-14 12:41:56 -0400

    made url into a JSON object

--- frontend/src/components/Thumbnail/Thumbnail.jsx
+++ frontend/src/components/Thumbnail/Thumbnail.jsx
@@ -3,7 +3,7 @@ import VideoThumbnail from 'react-video-thumbnail'
 const videoThumbnail = (props) => {
     return ( 
         <VideoThumbnail
-            videoUrl='https://www.youtube.com/watch?v=${video_ID}'
+            videoUrl={'https://www.youtube.com/watch?v=${props.video_ID}'}
             thumbnailHandler={(thumbnail) => console.log(thumbnail)}
             width={120}
             height={80}
